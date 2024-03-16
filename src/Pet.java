a83a0277b84480d8176a47484fa1025ccc44e5ab
BlessingChip38 <mattpythontest17@gmail.com>
2024-03-16 12:14:40 -0600

    Adds methods to increase and decrease health of Pet.

--- src/Pet.java
+++ src/Pet.java
@@ -5,19 +5,19 @@ public class Pet {
 
     public Pet() {
         this.name = "No Name";
-        this.health = 100;
+        this.health = 10;
         this.happiness = 100;
     }
 
     public Pet(String name) {
         this.name = name;
-        this.health = 100;
+        this.health = 10;
         this.happiness = 100;
     }
 
     /**
      * This method will increase the happiness of the pet by amount inputted.
-     * @param amount is the amount of increased happiness.
+     * @param amount Int that  increased happiness.
      */
     public void increaseHappiness(int amount) {
         //this will make sure happiness doesn't go ove 100
@@ -25,8 +25,8 @@ public class Pet {
     }
 
     /**
-     * This method will decrease the happiness of the pet by amoint inputted
-     * @param amount
+     * This method will decrease the happiness of the pet by amount inputted
+     * @param amount Int that decreases the happiness.
      */
     public void decreaseHappiness(int amount) {
         //this will make sure happiness doesn't go below 0
@@ -34,10 +34,29 @@ public class Pet {
     }
 
     /**
+     * This method will increase the health of the pet by amount inputted. Max health is 10 Min health is 0
+     * @param amount
+     */
+    public void increaseHealth(int amount) {
+        //this will make sure health doesn't go ove 10
+        this.health = Math.min(amount + this.health, 10);
+    }
+
+    /**
+     * This method will decrease the health of the pet by amount inputted. Max health is 10 Min health is 0
+     * @param amount
+     */
+    public void decreaseHealth(int amount) {
+        //this will make sure happiness doesn't go below 0
+        this.health = Math.max(this.health - amount, 0);
+    }
+
+    /**
      * This will change the pets name
      * @param name
      */
-    public void changeName(String name) {
+
+        public void changeName(String name) {
         this.name = name;
     }
 
