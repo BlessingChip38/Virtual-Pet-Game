b48f71b9ecbce7c5ca94913d91fc8b8c5e589e07
BlessingChip38 <mattpythontest17@gmail.com>
2024-03-16 13:29:53 -0600

    Adds additional methods for energy

---
+++ src/Activities.java
@@ -0,0 +1,20 @@
+public class Activities {
+    //This will hold the activities to increase and decrease attributes of Pet
+    private Pet pet;
+
+    public Activities(Pet pet) {
+        this.pet = pet;
+    }
+
+    public void feed() {
+
+    }
+
+    public void rest() {
+
+    }
+
+    public void play() {
+
+    }
+}
--- src/Pet.java
+++ src/Pet.java
@@ -2,70 +2,89 @@ public class Pet {
     private String name;
     private int health;
     private int happiness;
+    private int energy;
 
     public Pet() {
         this.name = "No Name";
         this.health = 10;
         this.happiness = 100;
+        this.energy = 100
     }
 
     public Pet(String name) {
         this.name = name;
         this.health = 10;
         this.happiness = 100;
+        this.energy = 100;
     }
 
     /**
      * This method will increase the happiness of the pet by amount inputted.
-     * @param amount Int that  increased happiness.
+     * @param amount Int that increased happiness.
      */
     public void increaseHappiness(int amount) {
         //this will make sure happiness doesn't go ove 100
-        this.happiness = Math.min(amount + this.happiness, 100);
+        happiness = Math.min(amount + happiness, 100);
     }
 
     /**
-     * This method will decrease the happiness of the pet by amount inputted
-     * @param amount Int that decreases the happiness.
+     * This method will decrease the happiness of the pet by given amount. Max happiness 100, Min happiness 0
+     * @param amount Int that decreases happiness.
      */
     public void decreaseHappiness(int amount) {
         //this will make sure happiness doesn't go below 0
-        this.happiness = Math.max(this.happiness - amount, 0);
+        happiness = Math.max(happiness - amount, 0);
     }
 
     /**
      * This method will increase the health of the pet by amount inputted. Max health is 10 Min health is 0
-     * @param amount
+     * @param amount Int that increases health
      */
     public void increaseHealth(int amount) {
         //this will make sure health doesn't go ove 10
-        this.health = Math.min(amount + this.health, 10);
+        health = Math.min(amount + health, 10);
     }
 
     /**
      * This method will decrease the health of the pet by amount inputted. Max health is 10 Min health is 0
-     * @param amount
+     * @param amount Int that decreases health
      */
     public void decreaseHealth(int amount) {
         //this will make sure happiness doesn't go below 0
-        this.health = Math.max(this.health - amount, 0);
+       health = Math.max(health - amount, 0);
     }
 
-    /**
-     * This will change the pets name
-     * @param name
-     */
 
-        public void changeName(String name) {
+
+    public void increaseEnergy(int amount) {
+        energy = Math.min(amount, 100);
+    }
+
+    public void decreaseEnergy(int amount) {
+        energy = Math.max(amount, 0);
+    }
+    
+    /**
+     * This will change the pets name. Must only contain letters
+     * @param name String for given pet name
+     */
+    public void changeName(String name) {
         this.name = name;
     }
 
     public int getHappiness() {
-        return this.happiness;
+        return happiness;
     }
 
     public String getName() {
-        return this.name;
+        return name;
     }
 
+    public int getHealth() {
+        return health;
+    }
+
+    public int getEnergy() {
+        return energy;
+    }
 }
